a8dee1fe9cdb466919e26d6f77887aa80b7cdafd
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2023-11-14 10:35:42 -0300

    Update Projeto.c

--- Projeto.c
+++ Projeto.c
@@ -87,4 +87,118 @@ void alterar(Pessoa p[], int totalCadastrados, int id) {
                         break;
 
                     case 2:
-                        printf("\n2 -
+                        printf("\n2 - Alterar idade:");
+                        scanf("%d", &p[i].idade);
+                        limparBuffer();
+                        break;
+
+                    case 3:
+                        printf("Alterar status 3 - Ativo / 0 - Inativo: ");
+                        scanf("%d", &p[i].status);
+                        limparBuffer();
+                        break;
+
+                    case 4:
+                        printf("Alterar nome:\n");
+                        fgets(p[i].nome, sizeof(p[i].nome), stdin);
+
+                        printf("\nAlterar idade:");
+                        scanf("%d", &p[i].idade);
+                        limparBuffer();
+
+                        printf("Alterar status 1 - Ativo / 0 - Inativo");
+                        scanf("%d", &p[i].status);
+                        limparBuffer();
+                        break;
+
+                    default:
+                        printf("Opcao invalida.\n");
+                }
+
+                return;
+            }
+        }
+    }
+    //nao achei na lista
+    printf("Nao existe o registro na lista.\n");
+}
+
+void excluir(Pessoa p[], int *totalCadastrados, int id) {
+    if (*totalCadastrados == 0) {
+        printf("Lista vazia\n");
+        return;
+    } else {
+        for (int i = 0; i < *totalCadastrados; i++) {
+            if (p[i].id == id) {
+                // Move o último elemento para a posição do elemento a ser excluído
+                p[i] = p[*totalCadastrados - 1];
+                (*totalCadastrados)--;
+                printf("\nRegistro excluido com sucesso.\n");
+                return;
+            }
+        }
+    }
+    printf("Nao existe o registro na lista.\n");
+}
+
+int main() {
+    Pessoa p[TAM];
+
+    int opcao, totalCadastrados = 0, buscaID;
+
+    do {
+        printf("\nPara cadastrar, digite 1.\n");
+        printf("Para visualizar o cadastro, digite 2.\n");
+        printf("Para buscar um cadastro, digite 3.\n");
+        printf("Para alterar um cadastro, digite 4.\n");
+        printf("Para excluir um cadastro, digite 5.\n");
+        printf("Para sair, digite 0.\n");
+
+        printf("\n\n");
+
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                if (totalCadastrados < TAM) {
+                    cadastrar(p, &totalCadastrados);
+                } else {
+                    printf("\nTotal de cadastros atingido.\n");
+                }
+                break;
+
+            case 2:
+                if (totalCadastrados == 0) {
+                    printf("\nNao existem pessoas na lista\n");
+                } else {
+                    imprimirLista(p, totalCadastrados);
+                }
+                break;
+
+            case 3:
+                printf("\nDigite o id para buscar: ");
+                scanf("%d", &buscaID);
+                buscar(p, totalCadastrados, buscaID);
+                break;
+
+            case 4:
+                printf("\nDigite o id para alterar: ");
+                scanf("%d", &buscaID);
+                alterar(p, totalCadastrados, buscaID);
+                break;
+
+            case 5:
+                printf("\nDigite o id para excluir: ");
+                scanf("%d", &buscaID);
+                excluir(p, &totalCadastrados, buscaID);
+                break;
+
+            default:
+                if (opcao != 0) {
+                    printf("Opcao invalida.\n");
+                }
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
