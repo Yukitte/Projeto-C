60007ca521134cd504f9f6e04c91573d085b5e9b
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2023-12-08 15:36:49 -0300

    Update atividadeAvaliativa3.c

--- atividadeAvaliativa3.c
+++ atividadeAvaliativa3.c
@@ -1,181 +1,175 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+typedef struct Produto{
+    char descricao[50];
+    int quantidade;
+    int codigo;
+    float valor;
+    int id;
+    struct Produto *prox;
+}Produto;
 
-struct Produto {
-  int codigo;
-  char nome[50];
-  int quantidade;
-  float preco;
-};
-
-struct ControleEstoque {
-  struct Produto *produtos;
-  int capacidade;
-  int numProdutos;
-};
-
-void inicializar_sistema(struct ControleEstoque *controleEstoque,
-                         int capacidadeInicial) {
-  controleEstoque->produtos =
-      malloc(capacidadeInicial * sizeof(struct Produto));
-  controleEstoque->capacidade = capacidadeInicial;
-  controleEstoque->numProdutos = 0;
+Produto* criarListaVazia(){
+    return NULL;
 }
 
-void adicionar_produto(struct ControleEstoque *controleEstoque) {
-  if (controleEstoque->numProdutos < controleEstoque->capacidade) {
-    struct Produto novoProduto;
-
-    printf("Informe o código do produto: ");
-    scanf("%d", &novoProduto.codigo);
-
-    printf("Informe a descrição do produto: ");
-    scanf("%s", novoProduto.nome);
-
-    printf("Informe a quantidade do produto: ");
-    scanf("%d", &novoProduto.quantidade);
-
-    printf("Informe o valor do produto: ");
-    scanf("%f", &novoProduto.preco);
-
-    controleEstoque->produtos[controleEstoque->numProdutos++] = novoProduto;
-
-    printf("Produto adicionado com sucesso!\n");
-  } else {
-    printf("Capacidade máxima atingida. Não é possível adicionar mais "
-           "produtos.\n");
-  }
-}
-
-void imprimir_relatorio(struct ControleEstoque *controleEstoque) {
-  printf("Relatório de Produtos no Estoque:\n");
-  for (int i = 0; i < controleEstoque->numProdutos; i++) {
-    printf("Código: %d, Descrição: %s, Quantidade: %d, Valor: R$%.2f\n",
-           controleEstoque->produtos[i].codigo,
-           controleEstoque->produtos[i].nome,
-           controleEstoque->produtos[i].quantidade,
-           controleEstoque->produtos[i].preco);
-  }
-}
-
-void imprimir_relatorio_completo(struct ControleEstoque *controleEstoque) {
-  printf("Relatório Completo de Produtos no Estoque:\n");
-  printf("%-10s %-20s %-10s %-10s\n", "Código\n", "Descrição\n", "Quantidade\n",
-         "Valor\n");
-
-  for (int i = 0; i < controleEstoque->numProdutos; i++) {
-    printf("%-10d %-20s %-10d %-10.2f\n", controleEstoque->produtos[i].codigo,
-           controleEstoque->produtos[i].nome,
-           controleEstoque->produtos[i].quantidade,
-           controleEstoque->produtos[i].preco);
-  }
-}
-
-void pesquisar_produto(struct ControleEstoque *controleEstoque) {
-  int codigoPesquisa;
-  printf("Informe o código do produto a ser pesquisado: ");
-  scanf("%d", &codigoPesquisa);
-
-  for (int i = 0; i < controleEstoque->numProdutos; i++) {
-    if (controleEstoque->produtos[i].codigo == codigoPesquisa) {
-      printf("Produto encontrado:\n");
-      printf("Código: %d\n", controleEstoque->produtos[i].codigo);
-      printf("Descrição: %s\n", controleEstoque->produtos[i].nome);
-      printf("Quantidade: %d\n", controleEstoque->produtos[i].quantidade);
-      printf("Valor: R$%.2f\n", controleEstoque->produtos[i].preco);
-      return;
+//funcao para criar um novo registro
+Produto* criarProduto(){
+    Produto *novoProduto = (Produto*)malloc(sizeof(Produto));
+    if(novoProduto == NULL){
+        printf("Erro de alocacao de memoria");
+        exit(EXIT_FAILURE);
     }
-  }
+    novoProduto->prox = NULL;
+    return novoProduto;
+}
+Produto* cadastrar(Produto *lista){
+    srand(time(NULL));
+    Produto *novoProduto = criarProduto();
+    novoProduto->id = rand() % 100;
+    printf("Digite o codigo: ");
+    scanf("%d", &novoProduto->codigo);
+    fflush(stdin);
+    printf("Digite a descricao: ");
+    scanf("%s", novoProduto->descricao);
+    printf("Digite a quantidade: ");
+    scanf("%d", &novoProduto->quantidade);
+    printf("Digite o valor: ");
+    scanf("%f", &novoProduto->valor);
 
-  printf("Produto com código %d não encontrado.\n", codigoPesquisa);
+    //verificar se a lista é nula
+    if(lista == NULL){
+        return novoProduto;
+    }else{
+        Produto *atual = lista;
+        while(atual->prox != NULL){
+            atual = atual->prox;
+            
+        }
+        atual->prox = novoProduto;
+        return lista;
+        //atualizar a lista 
+    }
+}
+ void mostrarLista(Produto *lista){
+        Produto *atual = lista;
+        if(lista == NULL){
+            printf("\nLista nula\n");
+            return;
+        }
+        else{
+            while(atual != NULL){
+                printf("\nCodigo: %d", atual->codigo);
+                printf("Descricao: %s", atual->descricao);
+                printf("\nQuantidade: %d", atual->quantidade);
+                printf("\nValor: %f", atual->valor);
+                printf("\n");
+                atual = atual->prox;
+            }
+        }
+ }
+ Produto* buscarProduto(Produto *lista, int idBusca){
+    Produto *atual = lista;
+        while(atual != NULL){
+            if(atual->id == idBusca){
+                printf("\nCodigo: %d", atual->codigo);
+                printf("\nDescricao: %s", atual->descricao);
+                printf("\nQuatidade: %d", atual->quantidade);
+                printf("\nValor: %f", atual->valor);
+                return atual;
+            }
+            atual = atual->prox;
+        }
+    printf("Pessoa nao encontrada");
+    return NULL;
+ }
+
+void alterar(Produto *novoProduto){
+    printf("Digite o novo codigo");
+    fflush(stdin);
+    fgets(novoProduto->codigo, sizeof(novoProduto->codigo), stdin);
+    fflush(stdin);
+    printf("Digite a nova descricao");
+    fflush(stdin);
+    scanf("%s", novoProduto->descricao);
 }
 
-void pesquisar_produto_pelo_codigo(struct ControleEstoque *controleEstoque) {
-  int codigoPesquisa;
-  printf("Informe o código do produto a ser pesquisado: ");
-  scanf("%d", &codigoPesquisa);
+Produto* excluir(Produto *lista, int idBusca){
+    Produto *atual = lista;
+    Produto *anterior = NULL;
 
-  for (int i = 0; i < controleEstoque->numProdutos; i++) {
-    if (controleEstoque->produtos[i].codigo == codigoPesquisa) {
-      printf("Produto encontrado:\n");
-      printf("Código: %d\n", controleEstoque->produtos[i].codigo);
-      printf("Descrição: %s\n", controleEstoque->produtos[i].nome);
-      printf("Quantidade: %d\n", controleEstoque->produtos[i].quantidade);
-      printf("Valor: R$%.2f\n", controleEstoque->produtos[i].preco);
-      return;
+    while(atual != NULL && atual->id != idBusca){
+    	anterior = atual;
+        atual = atual->prox;
+        
     }
-  }
-
-  printf("Produto com código %d não encontrado.\n", codigoPesquisa);
+    if(atual != NULL){
+        if(anterior != NULL){ 
+            anterior->prox = atual->prox;
+        }else{
+            lista = atual->prox;
+        }
+        free(atual);
+        printf("Removido com sucesso");
+    }else{
+        printf("Produto nao encontrado");
+    }
+    return lista;
 }
 
-void remover_produto(struct ControleEstoque *controleEstoque) {
-  int codigoRemocao;
-  printf("Informe o código do produto a ser removido: ");
-  scanf("%d", &codigoRemocao);
-
-  for (int i = 0; i < controleEstoque->numProdutos; i++) {
-    if (controleEstoque->produtos[i].codigo == codigoRemocao) {
-      for (int j = i; j < controleEstoque->numProdutos - 1; j++) {
-        controleEstoque->produtos[j] = controleEstoque->produtos[j + 1];
-      }
-      controleEstoque->numProdutos--;
-
-      printf("Produto removido com sucesso!\n");
-      return;
+void liberarLista(Produto *lista){
+    Produto *atual = lista;
+    Produto *prox;
+    while(atual != NULL){
+        prox = atual->prox;
+        free(atual);
+        atual = prox;
     }
-  }
-
-  printf("Produto com código %d não encontrado. Não foi possível remover.\n",
-         codigoRemocao);
 }
 
-int main() {
-  struct ControleEstoque controleEstoque;
-  inicializar_sistema(&controleEstoque, 10);
+int main(){
+    int opcao;
+    int idBusca;
+    Produto *encontrada;
+    Produto *lista = criarListaVazia();
+    do{
+        printf("\nDigite 1 para adicionar codigo");
+        printf("\nDigite 2 para mostrar");
+        printf("\nDigite 3 para buscar");
+        printf("\nDigite 4 para alterar");
+        printf("\nDigite 5 para excluir");
+        printf("\nDigite 0 para sair\n");
+        scanf("%d", &opcao);
+        switch(opcao){
+            case 1:
+                lista = cadastrar(lista);
+            break;
+            case 2:
+                mostrarLista(lista);
+            break;
+            case 3:
+                printf("Digite o id para busca");
+                scanf("%d", &idBusca);
+                encontrada = buscarProduto(lista, idBusca);
+            break;
+            case 4:
+                printf("\nDigite o id para alteracao");
+                scanf("%d", &idBusca);
+                encontrada = buscarProduto(lista, idBusca);
+                if(encontrada != NULL){
+                    alterar(encontrada);
+                }
+            break;
+            case 5:
+                printf("\nDigite o id para exclusao");
+                scanf("%d", &idBusca);
+                lista = excluir(lista, idBusca);
+            break;
+        }
+    }while(opcao != 0);
 
-  int opcao;
+    liberarLista(lista);
 
-  do {
-    printf("\n*** Menu ***\n");
-    printf("1. Adicionar Produto\n");
-    printf("2. Imprimir Relatório\n");
-    printf("3. Imprimir Relatório Completo\n");
-    printf("4. Pesquisar Produto\n");
-    printf("5. Pesquisar Produto pelo Código\n");
-    printf("6. Remover Produto\n");
-    printf("7. Sair\n");
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
-    case 1:
-      adicionar_produto(&controleEstoque);
-      break;
-    case 2:
-      imprimir_relatorio(&controleEstoque);
-      break;
-    case 3:
-      imprimir_relatorio_completo(&controleEstoque);
-      break;
-    case 4:
-      pesquisar_produto(&controleEstoque);
-      break;
-    case 5:
-      pesquisar_produto_pelo_codigo(&controleEstoque);
-      break;
-    case 6:
-      remover_produto(&controleEstoque);
-      break;
-    case 7:
-      printf("Saindo do programa. Obrigado!\n");
-      break;
-    default:
-      printf("Opção inválida. Tente novamente.\n");
-    }
-  } while (opcao != 7);
-
-  free(controleEstoque.produtos);
-
-  return 0;
+return 0;
 }
